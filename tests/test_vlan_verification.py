b26a0dd56f243c4ed93cfc3e90827237d7d2d5af
backupsrv <backupsrv@gmail.com>
2024-10-30 15:04:40 +0330

    pyats

--- tests/test_vlan_verification.py
+++ tests/test_vlan_verification.py
@@ -5,6 +5,7 @@ from unicon.core.errors import ConnectionError
 import logging
 from pyats.log.utils import banner
 
+
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
